302436b514c167ea08414779c19be11103d5c1cc
feritarou <schwarz.f@web.de>
2024-08-24 23:03:06 +0200

    fix: show added/copied/deleted/modified/renamed files

--- script.sh
+++ script.sh
@@ -9,3 +9,23 @@ echo "Processing added files"
 for file in ${ADDED_FILES}; do
   echo "- $file ..."
 done
+
+echo "Processing copied files"
+for file in ${COPIED_FILES}; do
+  echo "- $file ..."
+done
+
+echo "Processing modified files"
+for file in ${MODIFIED_FILES}; do
+  echo "- $file ..."
+done
+
+echo "Processing deleted files"
+for file in ${DELETED_FILES}; do
+  echo "- $file ..."
+done
+
+echo "Processing renamed files"
+for file in ${RENAMED_FILES}; do
+  echo "- $file ..."
+done
